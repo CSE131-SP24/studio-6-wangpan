f36cdbbbedff78c5c4e0b1d2b932e073df0e38bc
laurelwang <laurelwang@localhost>
2024-03-17 13:36:12 -0500

    studio 6

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,11 +12,16 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
+		if (n == 0) {
 			return 0;
-		
+
+		} else {
+
+			return 1 / Math.pow(2, n) + geometricSum(n-1);
+		}
 	}
+	
+	
 
 	/**
 	 * This method uses recursion to compute the greatest common divisor
@@ -27,13 +32,13 @@ public class RecursiveMethods {
 	 * @return greatest common divisor of p and q
 	 */
 	public static int gcd(int p, int q) {
-		
-			// FIXME compute the gcd of p and q using recursion
-			return 0;
-		
+
+		// FIXME compute the gcd of p and q using recursion
+		return 0;
+
 	}
 
-	
+
 
 	/**
 	 * This method uses recursion to create a reverse of the given array
@@ -42,10 +47,27 @@ public class RecursiveMethods {
 	 * @return an array with the same data as the input but it reverse order
 	 */
 	public static int[] toReversed(int[] array) {
-		
-			// FIXME create a helper method that can recursively reverse the given array
-			return new int[0];
-		
+		int[] reversed = new int[array.length];
+		if (array.length > 0) {
+			return reverser(array, reversed, 0);
+		} else {
+			return reversed;
+			
+		}
+		// FIXME create a helper method that can recursively reverse the given array
+		//return new int[0];
+	}
+	
+	public static int[] reverser(int [] array, int [] reversed, int index) {
+		if(index > array.length / 2) {
+			return reversed;
+			
+		} else {
+			int mirror = array.length - 1 - index;
+			reversed[index] = array[mirror];
+			reversed[mirror] = array[index];
+			return reverser(array, reversed, index + 1);
+		}
 	}
 
 	/**
@@ -57,9 +79,22 @@ public class RecursiveMethods {
 	 *                                      depth
 	 * @param radiusMinimumDrawingThreshold radius above which drawing should occur
 	 */
-	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
-			double radiusMinimumDrawingThreshold) {
-		
+	public static void circlesUponCircles(double xCenter, double yCenter, double radius, double radiusMinimumDrawingThreshold) {
+		double min = radiusMinimumDrawingThreshold;	
+		if (radius < radiusMinimumDrawingThreshold) {
+				return;
+			} else {
+				//xCenter = xCenter/3.0;
+				//yCenter = yCenter/3.0;
+				
+					StdDraw.circle(xCenter, yCenter, radius);
+					circlesUponCircles(xCenter - radius, yCenter, radius / 3.0, min);
+					circlesUponCircles(xCenter, yCenter - radius, radius / 3.0, min);
+					circlesUponCircles(xCenter + radius, yCenter, radius / 3.0, min);
+					circlesUponCircles(xCenter, yCenter + radius, radius / 3.0, min);
+				
+				
+			}
 		// FIXME
 	}
 
